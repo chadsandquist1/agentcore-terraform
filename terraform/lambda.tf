4741042790190f25d8b81be2c846e16aa1357014
Chad Sandquist <chadsandquist@msn.com>
2026-04-18 11:33:19 -0500

    fix lambda size by s3 backing

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -13,10 +13,20 @@ data "archive_file" "lambda_function" {
   }
 }
 
+resource "aws_s3_object" "lambda_layer" {
+  bucket = aws_s3_bucket.agent_code.id
+  key    = "lambda_layer.zip"
+  source = "${path.module}/../build/lambda_layer.zip"
+  etag   = filemd5("${path.module}/../build/lambda_layer.zip")
+
+  depends_on = [aws_s3_bucket_versioning.agent_code]
+}
+
 resource "aws_lambda_layer_version" "langchain" {
   layer_name               = "${var.prefix}-langchain"
-  filename                 = "${path.module}/../build/lambda_layer.zip"
-  source_code_hash         = filebase64sha256("${path.module}/../build/lambda_layer.zip")
+  s3_bucket                = aws_s3_bucket.agent_code.bucket
+  s3_key                   = aws_s3_object.lambda_layer.key
+  s3_object_version        = aws_s3_object.lambda_layer.version_id
   compatible_runtimes      = ["python3.12"]
   compatible_architectures = ["x86_64"]
 }
